55ac336a7922a330b3397bff04fe4fb20ebfb1fe
martamazurkiewicz <marta@mazurkiewi.cz>
2020-06-12 11:48:21 +0200

    Wartosci wlasne metoda Jacobiego

---
+++ Mazurkiewicz_Marta_Jacobi_obroty.m
@@ -0,0 +1,38 @@
+function x = Mazurkiewicz_Marta_Jacobi_obroty(A, eps)
+n = length(A);
+warunek = 2*eps;
+while warunek > eps
+  warunek = 0;
+  p = 0;
+  q = 0;
+  max = -inf;
+  for (i=1:n)
+    for (j=1:n)
+      if (i != j)
+        warunek += abs(A(i,j));
+        if (abs(A(i,j)) > max)
+          max = abs(A(i,j));
+          p = j;
+          q = i;
+        endif
+      endif
+    endfor
+  endfor
+  if (A(p,p) == A(q,q))
+    theta = pi/4;
+  else
+    tan2theta = (2*A(p,q))/(A(p,p)-A(q,q));
+    theta = atan(tan2theta)/2;
+  endif
+  T = eye(n);
+  T(p,p) = cos(theta);
+  T(p,q) = sin(theta);
+  T(q,p) = -sin(theta);
+  T(q,q) = cos(theta);
+  Tinv = T;
+  Tinv(q,p) = Tinv(q,p)*(-1);
+  Tinv(p,q) = Tinv(p,q)*(-1);
+  A = Tinv * A * T;
+endwhile
+x = diag(A);
+endfunction
--- test.m
+++ test.m
@@ -1,36 +1,4 @@
-clear;
-clear all;
-%A = [1,2;1,2;3,2;3,5];
-%B = [1,2,3;1,4,4];
-%MnozenieMacierzy(A,B);
-%A = [9,11,12,6,4];
-%c = 4;
-%[ B,R ] = Horner(A,c);
-%B = zeros(1,3)
-
-%L = [1,0,0;2,3,0;2,3,4];
-%b = [2,4,3];
-%x = uklad_L(L,b);
-
-%U = [2,4,4;0,2,3;0,0,1];
-%b = [2,12,3];
-%x = uklad_U(U,b);
-
-%x = [-1,0,1,2];
-%y = [4,-1,0,7];
-%n = 2;
-%x = [-1,2,5];
-%y = [5,-4,-13];
-%n = 1;
-%[suma] = Mazurkiewicz_Marta_MNK(x,y,n);
-%[a] = Mazurkiewicz_Marta_styczne(1,0.000001,@(x)(2*x^2-4*x+2));
-%[a] = Mazurkiewicz_Marta_styczne(-pi/4,0.000001,@sin);
-%[a] =Mazurkiewicz_Marta_sieczne(2,1,0.000001,@(x)(x^3-2*x^2-2));
-
-% V w kolumnach wektory wlasne
-%przykladowa macierz
-A  = [2, -3, 2; -3, -4, -2; 1, -2, 4];
+A = [1 2 3; 2 2 4 ; 3 4 3];
+x = Mazurkiewicz_Marta_Jacobi_obroty(A,0.01);
+x
 eig(A)
-b = [1;1;1];
-%poly(A)
-[x] = Mazurkiewicz_Marta_metoda_potegowa(A,b,0.10)
